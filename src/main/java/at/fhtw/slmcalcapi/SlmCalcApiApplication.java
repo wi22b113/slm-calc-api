57a35a5830dec0f3fef84d2b10f644b9552e6411
matthias.teuschl <teuschl@artindustrial-it.com>
2023-06-06 18:45:25 +0200

    intial commit

---
+++ src/main/java/at/fhtw/slmcalcapi/SlmCalcApiApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmcalcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmCalcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmCalcApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/slmcalcapi/SlmCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmcalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmCalcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
