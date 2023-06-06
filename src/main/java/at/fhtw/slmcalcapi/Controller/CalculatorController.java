ca4f28eabcc0cdd83a947edf7ed0b5d11007ad55
matthias.teuschl <teuschl@artindustrial-it.com>
2023-06-06 19:30:53 +0200

    intial commit

---
+++ src/main/java/at/fhtw/slmcalcapi/Controller/CalculatorController.java
@@ -0,0 +1,26 @@
+package at.fhtw.slmcalcapi.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/calc/add")
+    public int add (
+            @RequestParam int a,
+            @RequestParam int b
+    ){
+        return a+b;
+    }
+
+    @RequestMapping("/calc/x")
+    public int x (
+            @RequestParam int a,
+            @RequestParam int b
+    ){
+        return a+b;
+    }
+
+}
